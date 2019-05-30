407e34bf89aced57b27ff4d50699db10abebf23b
mawei <maw@77ircloud.com>
2019-05-30 15:02:58 +0800

    加入semantic-ui

--- src/component/AppList.js
+++ src/component/AppList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { observer, inject} from 'mobx-react';
+import { Input, Button, Grid} from 'semantic-ui-react';
 
 @inject('todos')
 @observer
@@ -28,9 +29,7 @@ class AppList extends React.Component{
         {
           todos.titleList&&
           todos.titleList.length>0&&
-          // todos.titleList.map(i => <div key={i.text} style={i.isDone?{color: '#CCC', textDecoration: 'line-through'}:{}} onClick={() => todos.changeDoneStatus(i.text)}>{i.text}</div>)
           todos.titleList.map(i => (
-            // console.log(i.id)
             <div
               key={i.id&&i.id}
               style={i.isDone?{color: '#CCC', textDecoration: 'line-through'}:{}}
@@ -40,8 +39,42 @@ class AppList extends React.Component{
             </div>
           ))
         }
-        <input onChange={this.changeText} placeholder="输入todo"></input>
-        <button onClick={()=>this.addTitle(this.state.addText)}>add</button>
+        <Input onChange={this.changeText} placeholder="输入todo" />
+        <Button content='add' onClick={()=>this.addTitle(this.state.addText)} />
+
+        <div>
+          <Grid columns='three' divided>
+            <Grid.Row>
+              <Grid.Column>
+                人间失格<br/>
+                太宰治
+              </Grid.Column>
+              <Grid.Column>
+                人间失格<br/>
+                太宰治
+              </Grid.Column>
+              <Grid.Column>
+                人间失格<br/>
+                太宰治
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+              <Grid.Column>
+                人间失格<br/>
+                太宰治
+              </Grid.Column>
+              <Grid.Column>
+                人间失格<br/>
+                太宰治
+              </Grid.Column>
+              <Grid.Column>
+                人间失格<br/>
+                太宰治
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
       </React.Fragment>
 
     )
